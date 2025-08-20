d17d36af552f74dc2de35b5e9d0cd16bc61e7482
bri <284789+b-@users.noreply.github.com>
2025-08-20 01:08:22 -0400

    add this

---
+++ this.tf
@@ -0,0 +1,18 @@
+resource "spacelift_stack" "spacelift-spacelift" {
+  name = "spacelift-spacelift"
+  space_id = "root"
+
+  repository = "spacelift-spacelift"
+  branch = "main"
+
+  terraform_version = "1.10.5"
+  terraform_workflow_tool = "OPEN_TOFU"
+  terraform_external_state_access = true
+  terraform_smart_sanitization = true
+
+  description = "spacelift stack to manage spacelift"
+  autodeploy = true
+  administrative = true
+  enable_well_known_secret_masking = true
+  github_action_deploy = false
+}
